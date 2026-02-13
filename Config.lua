f7576302a2fe82844ff0b16fa9e426eb4b417e69
Michael Zaerr <31021055+Thaonnor@users.noreply.github.com>
2026-02-13 15:45:29 -0500

    Update project structure by adding initial plan and .gitignore file

---
+++ Config.lua
@@ -0,0 +1,15 @@
+--[[
+-- Granite - Config (options UI)
+-- Copyright (C) 2025 Thaonnor
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY. See the COPYING file for full license text.
+]]
+
+-- Options will be registered here (AceConfig-3.0) once we add the UI.
+-- For now this file is a stub so the TOC load order is valid.
